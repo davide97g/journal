60ccb0c5ff88dd36628e73bd08766bd64b46ea8d
Davide Ghiotto <davide.ghiotto@bitrock.it>
2023-07-29 01:04:03 +0200

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,28 @@ export default defineConfig({
           importStyle: false, // css in js
         }),
       ],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Moodwrite",
+        short_name: "Moodwrite",
+        description: "A simple app to track your mood and journal",
+        theme_color: "#A276AE",
+        icons: [
+          {
+            src: "/images/ios/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/ios/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
 });
